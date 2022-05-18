02036bb42d10b0a6160ccc556ee980a46bac34d3
caninojories <caninojories@hotmail.com>
2022-05-18 08:16:12 +0800

    Add code formatting

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const csvFilePaths = ['./files/csv/users1.csv', './files/csv/users2.csv']
 const PREFIX_TO_REMOVE = 'ACN'
 
 interface JsonItem {
-	first_name: string
+  first_name: string
   last_name: string
   user: string
   email: string
@@ -39,9 +39,11 @@ export class User {
 }
 
 const run = async () => {
-	const results: User[][] =  await Promise.all(csvFilePaths.map(async (csvFilePath) => {
-    return transformCsvToJson(csvFilePath)
-  }))
+  const results: User[][] = await Promise.all(
+    csvFilePaths.map(async (csvFilePath) => {
+      return transformCsvToJson(csvFilePath)
+    }),
+  )
 
   let users: User[] = results.reduce((acc, curVal) => {
     return acc.concat(curVal)
@@ -54,35 +56,35 @@ const transformCsvToJson = async (csvFilePath: string): Promise<User[]> => {
   const userData: User[] = []
 
   await csv({
-		delimiter: '||'
-	})
-	.fromFile(csvFilePath)
-	.subscribe((json: any) => {
-		const item: JsonItem = json
-		return new Promise((resolve) => {
-			const user = new User()
-      const phone = parseToInt(item.phone.replace(/[^\d+]+/g, ''))
-      const dateSplit = item.date.split('/')
-      const date = `${dateSplit[2]}-${dateSplit[1]}-${dateSplit[0]}`
-      const costCenterNum = item.cc.replace(PREFIX_TO_REMOVE, '')
+    delimiter: '||',
+  })
+    .fromFile(csvFilePath)
+    .subscribe((json: any) => {
+      const item: JsonItem = json
+      return new Promise((resolve) => {
+        const user = new User()
+        const phone = parseToInt(item.phone.replace(/[^\d+]+/g, ''))
+        const dateSplit = item.date.split('/')
+        const date = `${dateSplit[2]}-${dateSplit[1]}-${dateSplit[0]}`
+        const costCenterNum = item.cc.replace(PREFIX_TO_REMOVE, '')
 
-			user.assimilate({
-				name: `${item.first_name} ${item.last_name}`,
-				phone,
-				person: {
-					firstName: item.first_name,
-					lastName: item.last_name
-				},
-				amount: parseFloat(item.amount),
-				date,
-				costCenterNum,
-			})
+        user.assimilate({
+          name: `${item.first_name} ${item.last_name}`,
+          phone,
+          person: {
+            firstName: item.first_name,
+            lastName: item.last_name,
+          },
+          amount: parseFloat(item.amount),
+          date,
+          costCenterNum,
+        })
 
-      userData.push(user)
+        userData.push(user)
 
-			resolve()
-		})
-	})
+        resolve()
+      })
+    })
 
   return userData
 }
